d3ad782658161c189b45bd60ea6bf965dee699aa
Doria May <112048607+DoriaMay@users.noreply.github.com>
2023-07-26 10:44:08 +0530

    Api image

--- main.js
+++ main.js
@@ -23,37 +23,37 @@ const submitIcon = document.querySelector("#submit-icon");
 const inputElement = document.querySelector("input");
 const imageSection = document.querySelector(".images-section")
 
-const getImages = async() => {
-    const options = {
-        method: "POST",
-        headers: {
-            "Authorization": `Bearer ${API_KEY}`,
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            prompt: inputElement.value,
-            n: 4,
-            size: "1024x1024"
-        })
-    }
-    try {
-       const response = await fetch("https://api.openai.com/v1/images/generations", options)
-       const data = await response.json()
-       console.log(data)
+// const getImages = async() => {
+//     const options = {
+//         method: "POST",
+//         headers: {
+//             "Authorization": `Bearer ${API_KEY}`,
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify({
+//             prompt: inputElement.value,
+//             n: 4,
+//             size: "1024x1024"
+//         })
+//     }
+//     try {
+//        const response = await fetch("https://api.openai.com/v1/images/generations", options)
+//        const data = await response.json()
+//        console.log(data)
 
-       data?.data.forEach(imageObject => {
-        const imageContainer = document.createElement("div")
-        imageContainer.classList.add("image-container")
-        const imgageElement = document.createElement("img")
-        imgageElement.setAttribute('src', imageObject.url)
-        imageContainer.append(imgageElement)
-        imageSection.append(imageContainer)
-       })
+//        data?.data.forEach(imageObject => {
+//         const imageContainer = document.createElement("div")
+//         imageContainer.classList.add("image-container")
+//         const imgageElement = document.createElement("img")
+//         imgageElement.setAttribute('src', imageObject.url)
+//         imageContainer.append(imgageElement)
+//         imageSection.append(imageContainer)
+//        })
 
-    } catch (error) {
-        console.error(error)
-    }
-}
+//     } catch (error) {
+//         console.error(error)
+//     }
+// }
 
 // image
 document.querySelectorAll('.imge-container img').forEach(image => {
